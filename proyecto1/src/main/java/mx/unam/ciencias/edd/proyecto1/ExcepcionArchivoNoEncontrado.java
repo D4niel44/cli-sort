a916b446a8b7850615c18394497c70785672544c
Daniel Linares Gil <daniel44@main-pc.myhost>
2020-03-26 20:13:48 -0600

    Excepcion Nueva

---
+++ proyecto1/src/main/java/mx/unam/ciencias/edd/proyecto1/ExcepcionArchivoNoEncontrado.java
@@ -0,0 +1,32 @@
+package mx.unam.ciencias.edd.proyecto1;
+
+import java.io.IOException;
+
+/**
+ * Excepcion que se lanza cuando una bandera que debe recibir un argumento no lo
+ * recibe o recibe uno inválido.
+ */
+public class ExcepcionArchivoNoEncontrado extends RuntimeException {
+
+    /**
+     * Constructor que no recibe parámetros
+     */
+    public ExcepcionArchivoNoEncontrado() {}
+
+    /**
+     * Constructor que recibe un mensaje.
+     * @param mensaje Mensaje recibido
+     */
+    public ExcepcionArchivoNoEncontrado(String mensaje) {
+        super(mensaje);
+    }
+
+    /**
+     * Constructor que recibe un mensaje y una causa.
+     * @param mensaje Mensaje
+     * @param c Causa de la excepcion.
+     */
+    public ExcepcionArchivoNoEncontrado(String mensaje, Throwable c) {
+        super(mensaje, c);
+    }
+}
